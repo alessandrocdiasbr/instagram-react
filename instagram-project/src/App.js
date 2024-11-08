c25551fd0bdeffbb6478e7ba5af8e2c9440ca0ed
Alessandro <acdias83@gmail.com>
2024-11-08 14:20:20 -0300

    Index e App criado

---
+++ instagram-project/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Body from "./Body";
+import NavBar from "./NavBar";
+
+export default function App() {
+    return (
+        <div className="root">
+            <NavBar />
+            <Body />
+        </div>
+    );
+}
